d2ff013dc3433fbdfbd4d672aeed640d2bf600d2
davoryunsavory <david.marino@students.mrhschools.net>
2021-08-15 20:35:54 -0500

    8/15/2021 8:35pm

--- Main.java
+++ Main.java
@@ -3,42 +3,64 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;  
+import java.util.Scanner;
+
 class Main {
 
-    public static void main(String[] args) throws Exception {
-        
-        File file = new File("C:\\Users\\david\\OneDrive\\Desktop\\stocks\\newdow.csv");
-        List<String> line = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+    private static List<Double> openArray = new ArrayList<Double>();
+    private static List<Double> closeArray = new ArrayList<Double>();
+    private static List<Double> diffArray = new ArrayList<Double>();
+    private static List<Double> slopeArray = new ArrayList<Double>(50);
+
+    public static List<Double> openFunction(File file, List<String> line) {
         String[] array = new String[line.size()];
-        List<Double> openArray = new ArrayList<Double>();
-        List<Double> closeArray = new ArrayList<Double>();
-        List<Double> diffArray = new ArrayList<Double>();
-        List<Double> slopeArray = new ArrayList<Double>();
-        for (String lines : line) { 
+        for (String lines : line) {
             array = lines.split(",");
             openArray.add(Double.parseDouble(array[1]));
-            closeArray.add(Double.parseDouble(array[4]));
-            //System.out.println(array[1]); 
-         }
-        for (int i = 1; i < openArray.size(); i++) {
-            diffArray.add(closeArray.get(i) - openArray.get(i));
         }
+        return openArray;
+    }
 
+    public static List<Double> closeFunction(File file, List<String> line) {
+
+        String[] array = new String[line.size()];
+        for (String lines : line) {
+            array = lines.split(",");
+            closeArray.add(Double.parseDouble(array[4]));
+        }
+        return closeArray;
+    }
+
+    public static List<Double> diffFuction(File file, List<String> line) {
+        String[] array = new String[line.size()];
+        for (String lines : line) {
+            array = lines.split(",");
+            diffArray.add(Double.parseDouble(array[4]) - Double.parseDouble(array[1]));
+        }
         for (int i = 0; i < diffArray.size(); i++) {
-            List<Double> tempCollection = new ArrayList<Double>();
-            while (diffArray.get(i) != null) {
-                if (diffArray.get(i) < 0) {
-                    break;
+            for (int k = 0; k < 100;) {
+                if (diffArray.get(i) >= 0 && diffArray.get(i + 1) <= 0) {
+                    k++;
+                    continue;
+                } else if (diffArray.get(i) <= 0 && diffArray.get(i + 1) >= 0) {
+                    k++;
+                    continue;
                 } else {
-                    tempCollection.add(diffArray.get(i));
+                    slopeArray.add(k, (slopeArray.get(k) + diffArray.get(i)) / i);
                 }
             }
         }
+        return diffArray;
+    }
 
-        System.out.println();
+    public static void main(String[] args) throws Exception {
+
+        File file = new File("C:\\Users\\david\\OneDrive\\Desktop\\stocks\\newdow.csv");
+        List<String> line = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+
+        System.out.println(diffFuction(file, line));
     }
 }
 
-
 // 1, 6, 8, 7
+// Date,Open,High,Low,Close,Adj Close,Volume
